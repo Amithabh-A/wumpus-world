676f0cd34b4f20e1d635998fc870f2a6d43fa794
Alexander Bachmann <44279087+alexander-bachmann@users.noreply.github.com>
2020-04-10 16:34:15 -0500

    added agent notes

--- agent.py
+++ agent.py
@@ -1,5 +1,9 @@
 class Agent:
     def __init__(self):
-        ...
+        self.world_knowledge = [[[]]]
 
-    
+    def move(self):
+        ...
+        # add human movement to test world_knowledge
+        # each move fills out world knowledge..
+        # Agent can only move adjacently but not out of bounds
