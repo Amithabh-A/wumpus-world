2a964d337cdc2c150c1643d4687cc1553b802d2a
Alexander Bachmann <44279087+alexander-bachmann@users.noreply.github.com>
2020-04-10 14:52:57 -0500

    added pretty pandas print (read comment in main.py for install instructions)

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from world import World
 from file_parser import File_Parser
 
+from pandas import * # pip install pandas  --  will eventually remove
+
 world = World()
 world.generate_world("world_1.txt")
+world.populate_indicators()
 
 print(world.world)
+
+print(DataFrame(world.world))
--- world.py
+++ world.py
@@ -3,6 +3,8 @@ from file_parser import File_Parser
 class World:
     def __init__(self):
         self.world = [[]]
+        self.num_rows = 0
+        self.num_cols = 0
         # print("World generated")
 
     def generate_world(self, file_name):
@@ -15,7 +17,10 @@ class World:
         print(file_parser.gold)
         print(file_parser.pits)
         """
-        self.world = [[0 for i in range(int(file_parser.row_col[1]))] for j in range(int(file_parser.row_col[0]))]
+        self.num_rows = int(file_parser.row_col[0])
+        self.num_cols = int(file_parser.row_col[1])
+
+        self.world = [[0 for i in range(self.num_cols)] for j in range(self.num_rows)]
         self.world[int(file_parser.agent[1])][int(file_parser.agent[2])] = file_parser.agent[0]
         self.world[int(file_parser.wumpus[1])][int(file_parser.wumpus[2])] = file_parser.wumpus[0]
         self.world[int(file_parser.gold[1])][int(file_parser.gold[2])] = file_parser.gold[0]
@@ -23,7 +28,24 @@ class World:
             self.world[int(pit[1])][int(pit[2])] = pit[0]
 
 
-
     def populate_indicators(self):
-        ...
+        
+        for i in range(self.num_rows):
+            for j in range(self.num_cols):
+
+                if self.world[i][j] == 'A':
+                    print("Agent at [" + str(i) + ", " + str(j) + "]")
+
+                if self.world[i][j] == 'W':
+                    print("Wumpus at [" + str(i) + ", " + str(j) + "]")
+
+                if self.world[i][j] == 'G':
+                    print("Gold at [" + str(i) + ", " + str(j) + "]")
+
+                if self.world[i][j] == 'P':
+                    print("Pit at [" + str(i) + ", " + str(j) + "]")
+
+
+
+
         # breezes, stenches
