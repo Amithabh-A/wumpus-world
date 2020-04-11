1fab1726149d7aa5d2ca2621b33b357d24923c2c
Alexander Bachmann <44279087+alexander-bachmann@users.noreply.github.com>
2020-04-11 13:17:29 -0500

    indicators now fill out as they are experienced

--- agent.py
+++ agent.py
@@ -1,6 +1,3 @@
-from pandas import *
-
-
 class Agent:
     def __init__(self, world):
         self.world = world
@@ -8,19 +5,48 @@ class Agent:
         self.world_knowledge[self.world.agent_row][self.world.agent_col].append('A')
 
     def move(self, direction):
-        ...
-        # add human movement to test world_knowledge
         # each move fills out world knowledge..
-        # Agent can only move adjacently but not out of bounds
-
+        # each move makes predictions based on indicators of where pits and wumpus are
         if direction == 'u':
-            return self.move_up()
+            successful_move = self.move_up()
+            self.add_indicators_to_knowledge()
+            return successful_move
         if direction == 'r':
-            return self.move_right()
+            successful_move = self.move_right()
+            self.add_indicators_to_knowledge()
+            return successful_move
         if direction == 'd':
-            return self.move_down()
+            successful_move = self.move_down()
+            self.add_indicators_to_knowledge()
+            return successful_move
         if direction == 'l':
-            return self.move_left()
+            successful_move = self.move_left()
+            self.add_indicators_to_knowledge()
+            return successful_move
+        return False
+
+    def add_indicators_to_knowledge(self):
+        if 'B' in self.world.world[self.world.agent_row][self.world.agent_col]:
+            if 'B' not in self.world_knowledge[self.world.agent_row][self.world.agent_col]:
+                self.world_knowledge[self.world.agent_row][self.world.agent_col].append('B')
+        if 'S' in self.world.world[self.world.agent_row][self.world.agent_col]:
+            if 'S' not in self.world_knowledge[self.world.agent_row][self.world.agent_col]:
+                self.world_knowledge[self.world.agent_row][self.world.agent_col].append('S')
+        if 'G' in self.world.world[self.world.agent_row][self.world.agent_col]:
+            if 'G' not in self.world_knowledge[self.world.agent_row][self.world.agent_col]:
+                self.world_knowledge[self.world.agent_row][self.world.agent_col].append('G')
+        if 'P' in self.world.world[self.world.agent_row][self.world.agent_col]:
+            if 'P' not in self.world_knowledge[self.world.agent_row][self.world.agent_col]:
+                self.world_knowledge[self.world.agent_row][self.world.agent_col].append('P')
+        if 'W' in self.world.world[self.world.agent_row][self.world.agent_col]:
+            if 'W' not in self.world_knowledge[self.world.agent_row][self.world.agent_col]:
+                self.world_knowledge[self.world.agent_row][self.world.agent_col].append('W')
+
+    def predict_pits(self):
+        ...
+    def predict_wumpus(self):
+        ...
+
 
 
     def move_up(self):
@@ -83,3 +109,14 @@ class Agent:
     def add_agent(self):
         self.world.world[self.world.agent_row][self.world.agent_col].append('A')
         self.world_knowledge[self.world.agent_row][self.world.agent_col].append('A')
+
+
+    def is_dead(self):
+        if 'W' in self.world.world[self.world.agent_row][self.world.agent_col]:
+            print("You have been slain by the Wumpus!")
+            return True
+        elif 'P' in self.world.world[self.world.agent_row][self.world.agent_col]:
+            print("You have fallen in a pit!")
+            return True
+        else:
+            return False
--- main.py
+++ main.py
@@ -5,16 +5,13 @@ from pandas import * # pip install pandas  --  will eventually remove
 
 world = World()
 world.generate_world("world_1.txt")
-
-print(DataFrame(world.world))
-
 agent = Agent(world)
-# print(agent.move('u'))
+
+# print(DataFrame(world.world))
 print(DataFrame(agent.world_knowledge))
 
-while True:
-
+while agent.is_dead() == False:
     direction = input("Move (u, r, d, l): ")
     agent.move(direction)
-    print(DataFrame(world.world))
+    # print(DataFrame(world.world))
     print(DataFrame(agent.world_knowledge))
