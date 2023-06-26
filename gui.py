2dba404a8edf2d1c91f7ec035183cc2395634147
Amithabh-A <amithabh2002@gmail.com>
2023-06-26 23:10:31 +0530

    Removed Buttons. Single file containing world information.

--- gui.py
+++ gui.py
@@ -32,18 +32,6 @@ world = World()
 world.generate_world("world_1.txt")
 label_grid = [[Grid_Label(master, i, j) for j in range(world.num_cols)] for i in range(world.num_rows)]
 # agent = Agent(world, label_grid)
-
-# start = Button(master, text="Start", command= lambda: solve_wumpus_world(master, "world_1.txt"))
-world_1 = Button(master, text="World 1",  command= lambda: solve_wumpus_world(master, "world_1.txt"), width=8, font = "Helvetica 14 bold", bg = "gray80", fg = "gray40", borderwidth=0, activeforeground="white", activebackground="gray40")
-world_2 = Button(master, text="World 2",  command= lambda: solve_wumpus_world(master, "world_2.txt"), width=8, font = "Helvetica 14 bold", bg = "gray80", fg = "gray40", borderwidth=0, activeforeground="white", activebackground="gray40")
-world_3 = Button(master, text="World 3",  command= lambda: solve_wumpus_world(master, "world_3.txt"), width=8, font = "Helvetica 14 bold", bg = "gray80", fg = "gray40", borderwidth=0, activeforeground="white", activebackground="gray40")
-world_4 = Button(master, text="World 4",  command= lambda: solve_wumpus_world(master, "world_4.txt"), width=8, font = "Helvetica 14 bold", bg = "gray80", fg = "gray40", borderwidth=0, activeforeground="white", activebackground="gray40")
-
-# start.grid(row = 0, column = len(label_grid[0]), sticky = W, pady = 1)
-world_1.grid(row = 0, column = len(label_grid[0]))
-world_2.grid(row = 1, column = len(label_grid[0]))
-world_3.grid(row = 2, column = len(label_grid[0]))
-world_4.grid(row = 3, column = len(label_grid[0]))
-
+solve_wumpus_world(master, "world_1.txt")
 
 mainloop()
