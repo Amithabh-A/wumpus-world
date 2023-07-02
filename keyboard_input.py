dd89c4b9e80fe728f309c9dd069e2ad9790d38d7
Amithabh-A <amithabh2002@gmail.com>
2023-07-02 15:13:28 +0530

    adding keyboard class module

---
+++ keyboard_input.py
@@ -0,0 +1,54 @@
+from tkinter import *
+
+class Keyboard_Input:
+
+    def __init__(self,master):
+        self.global_key_input = None
+        self.root = master
+        self.key_input = StringVar()
+        self.root.bind("<KeyPress>", self.on_key_press)
+        self.root.focus_set()
+        self.root.update()
+        #self.root.quit()
+
+    def on_key_press(self, event):
+        global global_key_input
+        if event.keysym == 'Up':
+            self.key_input.set("u")
+            print("up")
+            self.global_key_input = self.key_input.get()
+            self.root.quit()
+        elif event.keysym == 'Down':
+            print("down")
+            self.key_input.set("d")
+            self.global_key_input = self.key_input.get()
+            self.root.quit()
+        elif event.keysym == 'Left':
+            print("left")
+            self.key_input.set("l")
+            self.global_key_input = self.key_input.get()
+            self.root.quit()
+        elif event.keysym == 'Right':
+            print("right")
+            self.key_input.set("r")
+            self.global_key_input = self.key_input.get()
+            self.root.quit()
+        elif event.keysym == 'Escape':
+            print("Exiting application")
+            self.root.quit()
+        self.root.destroy()
+        #print("Key input:", global_key_input)
+    
+    def get_key(self):
+        return self.global_key_input
+
+
+
+
+
+
+#root.iconify()
+
+#root.update()
+
+#print("Global Key input:", global_key_input)
